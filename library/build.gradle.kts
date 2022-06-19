2faebf31e2b606745ff96a36a012f3cd823b01ba
mainrs <5113257+mainrs@users.noreply.github.com>
2022-06-19 21:11:39 +0200

    build: add group and version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     id("maven-publish")
 }
 
+group = "com.github.mainrs"
+version = "1.0.0"
+
 android {
     namespace = "net.zerotask.libraries.android.compose.furigana"
     compileSdk = libs.versions.compileSdk.get().toInt()
